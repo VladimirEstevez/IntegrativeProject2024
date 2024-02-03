61232533f03a46ef8d1835ccecd011edad025f5d
Vladimir <le0679993@crc-lennox.qc.ca>
2024-02-02 23:00:11 -0500

    got the webhook to work

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,11 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const { connect } = require('./database/database');
+const ngrok = require('ngrok');
+
+/*
+TO RUN SERVER DO NPM START AND ON ANOTHER TERMINAL DO NGROK HTTP 8080
+*/
 
 app.use(cors());
 app.use(morgan('tiny'));
@@ -13,8 +18,16 @@ app.get('/', function(req, res) {
     res.send('Hello World')
 })
 
+app.post('/', (req, res) => {
+    console.log("Webhook received:");
+    console.log(req.body);
+    res.status(200).send(req.body);
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-    connect()
-    console.log("Server listening on port " + port)
+    //connect()
+    console.log("Server listening on port " + port);
+
+    
 })
