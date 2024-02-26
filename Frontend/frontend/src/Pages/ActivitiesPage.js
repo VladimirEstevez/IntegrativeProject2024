95c7116f2d24247a0b7968ace1826ad90c774cf5
Munir <le2176022@crc-lennox.qc.ca>
2024-02-26 17:20:50 -0500

    modified the Card.js and the ActivitiesPage.js. I also formatted the date and time of the activity by a function

--- Frontend/frontend/src/Pages/ActivitiesPage.js
+++ Frontend/frontend/src/Pages/ActivitiesPage.js
@@ -1,73 +1,92 @@
-import React, { useEffect, useState } from 'react';
-import Card from '../Objects/Card';
-import { useNavigate } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BoxArrowInLeft } from 'react-bootstrap-icons';
+import React, { useEffect, useState } from "react";
+import Card from "../Objects/Card";
+import { useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BoxArrowInLeft } from "react-bootstrap-icons";
 
 const ActivitiesPage = () => {
   const [activities, setActivities] = useState([]);
   const navigate = useNavigate();
-  const token = localStorage.getItem('token');
-  
+  const token = localStorage.getItem("token");
+
   useEffect(() => {
-  
     const fetchProtectedRoute = async () => {
-    
       if (!token) {
-        navigate('/');
+        navigate("/");
       } else {
         try {
-          const response = await fetch('http://localhost:8080/user/protectedRoute', {
-            method: 'GET',
-            headers: {
-              authorization: 'Bearer ' + token,
-            },
-          });
+          const response = await fetch(
+            "http://localhost:8080/user/protectedRoute",
+            {
+              method: "GET",
+              headers: {
+                authorization: "Bearer " + token,
+              },
+            }
+          );
 
-          console.log('response: ', response);
+          console.log("response: ", response);
           if (response.status === 401) {
-            navigate('/');
-          } 
+            navigate("/");
+          }
         } catch (error) {
-          console.error('Error:', error);
+          console.error("Error:", error);
         }
       }
     };
 
     fetchProtectedRoute();
-  }, [navigate,token]);
+  }, [navigate, token]);
 
-  useEffect(() => {const fetchActivities = async () => {
-    try {
-      const response = await fetch('http://localhost:8080/activities');
-      const data = await response.json();
-      console.log('data: ', data);
-      setActivities(data);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };
+  useEffect(() => {
+    const fetchActivities = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/activities");
+        const data = await response.json();
+        console.log("data: ", data);
+        setActivities(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
 
-  fetchActivities();
+    fetchActivities();
   }, []);
   return (
-    <div className="container-fluid bg-white d-flex flex-column align-items-center justify-content-center" style={{ height: '100vh' }}>
-    <h1>Vos activités</h1>
-    <div className="d-flex justify-content-center">
-    {activities.map(activity => (
-  <Card
-    key={activity._id}
-    activity={activity}
-  />
-))}
-    </div>
-    <button onClick={() => navigate("/")} className="btn btn-primary mt-4" 
-      style={{ borderRadius: '10px', 
-      position: 'relative', padding: '10px 20px', transition: 'transform 0.3s' }}
-      onMouseEnter={(e) => { e.currentTarget.style.transform = 'scale(1.1)'; }}
-      onMouseLeave={(e) => { e.currentTarget.style.transform = 'scale(1)'; }}>
-        <span style={{ marginRight: '5px' }}>Se Déconnecter</span>
-            <BoxArrowInLeft size={24} />
+    <div
+      className="container-fluid bg-white d-flex flex-column align-items-center justify-content-center"
+      style={{ minHeight: '100vh', padding: '20px' }}
+    >
+      <div className="row justify-content-center">
+        <h1>Vos activités</h1>
+      </div>
+      <div className="col-10">
+        <div className="row">
+          {activities.map((activity) => (
+            <div className="m-4" key={activity._id}>
+              <Card activity={activity} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <button
+        onClick={() => navigate("/")}
+        className="btn btn-primary mt-4"
+        style={{
+          borderRadius: "10px",
+          position: "relative",
+          padding: "10px 20px",
+          transition: "transform 0.3s",
+        }}
+        onMouseEnter={(e) => {
+          e.currentTarget.style.transform = "scale(1.1)";
+        }}
+        onMouseLeave={(e) => {
+          e.currentTarget.style.transform = "scale(1)";
+        }}
+      >
+        <span style={{ marginRight: "5px" }}>Se Déconnecter</span>
+        <BoxArrowInLeft size={24} />
       </button>
     </div>
   );
