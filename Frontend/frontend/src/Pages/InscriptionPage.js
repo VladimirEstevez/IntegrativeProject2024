8cd8a9130daa6e40d1bddce7bb5c8207d16ee0a2
Munir <le2176022@crc-lennox.qc.ca>
2024-02-27 18:26:17 -0500

    repasted the useffect(I removed it in my last commit by accident)

--- Frontend/frontend/src/Pages/InscriptionPage.js
+++ Frontend/frontend/src/Pages/InscriptionPage.js
@@ -1,5 +1,5 @@
 // InscriptionPage.js
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { toast,ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { FilePerson, XLg } from 'react-bootstrap-icons';
@@ -99,7 +99,37 @@ function InscriptionPage() {
       // Handle error here
     }
   };
+  
+  useEffect(() => {
+    const fetchProtectedRoute = async () => {
+      const token = localStorage.getItem('token');
 
+      if (token) {
+        try {
+          const response = await fetch('http://localhost:8080/protectedRoute', {
+            method: 'GET',
+            headers: {
+              authorization: 'Bearer ' + token,
+            },
+          });
+
+          console.log('response: ', response);
+          if (response.status === 401) {
+            localStorage.deleteItem('token');
+            navigate('/');
+          } else {
+            const user = await response.json();
+            console.log('user: ', user);
+            navigate('/menu');
+          }
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      }
+    };
+    
+    fetchProtectedRoute();
+  }, [navigate]);
   
   return (
     <div><ToastContainer />
