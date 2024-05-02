bfe269b3d470eb6142f5d2034bcbcca99524e8fd
VladimirEstevez <131298848+VladimirEstevez@users.noreply.github.com>
2024-05-02 19:33:27 -0400

    fixing the navbar

--- Frontend/src/Objects/NavbarComponent.js
+++ Frontend/src/Objects/NavbarComponent.js
@@ -7,7 +7,7 @@ import {
   PersonGear,
   PersonWalking,
 } from "react-bootstrap-icons"; // Importing icons from react-bootstrap-icons
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 // Functional component for Navbar
 function NavbarComponent() {
@@ -25,53 +25,82 @@ function NavbarComponent() {
   // Rendering Navbar component
   return (
     <div>
-   <Navbar
-  style={{
-    backgroundColor: "white", 
-    zIndex: 1000,
-    borderBottom: '1px solid black' 
-  }}
-  expand="lg"
-  fixed="top"
-  className="d-flex justify-content-between align-items-start"
-  expanded={isNavbarOpen} // Add this line
->
+      <Navbar
+        style={{
+          backgroundColor: "white",
+          zIndex: 1000,
+          borderBottom: "1px solid black",
+        }}
+        expand="lg"
+        fixed="top"
+        className="d-flex justify-content-between align-items-start"
+        expanded={isNavbarOpen} // Add this line
+      >
         <div>
-        <Navbar.Toggle
-  onClick={() => setIsNavbarOpen(prevIsNavbarOpen => !prevIsNavbarOpen)}
-  aria-controls="basic-navbar-nav"
-/>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            {/* Link to view user's activities */}
-            <Nav.Link className="mx-2" onClick={() => { navigate("/myActivities"); setIsNavbarOpen(false); }}>
-  <span style={{ marginRight: "5px" }}>Voir mes activités</span>
-  <BookmarkHeart size={24} />
-</Nav.Link>
-<Nav.Link className="mx-2" onClick={() => { navigate("/activities"); setIsNavbarOpen(false); }}>
-  <span style={{ marginRight: "5px" }}>Voir les activités</span>
-  <PersonWalking size={24} />
-</Nav.Link>
-<Nav.Link className="mx-2" onClick={() => { navigate("/modify"); setIsNavbarOpen(false); }}>
-  <span style={{ marginRight: "5px" }}>Modifier Mon Profil</span>
-  <PersonGear size={24} />
-</Nav.Link>
-<Nav.Link className="mx-2" onClick={() => { SeDeconnecter(); setIsNavbarOpen(false); }}>
-  <span style={{ marginRight: "5px" }}>Se Déconnecter</span>
-  <BoxArrowInLeft size={24} />
-</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+          <Navbar.Toggle
+            className="btn-hover-effect mx-2 py-1"
+            onClick={() =>
+              setIsNavbarOpen((prevIsNavbarOpen) => !prevIsNavbarOpen)
+            }
+            aria-controls="basic-navbar-nav"
+          />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <div style={{ display: 'inline-block', width: 'fit-content' }}> 
+            <Nav className="ml-auto">
+              {/* Link to view user's activities */}
+              <Nav.Link
+                className="mx-2 btn my-1 btn-light btn-hover-effect border border-3 rounded"
+                onClick={() => {
+                  navigate("/myActivities");
+                  setIsNavbarOpen(false);
+                }}
+              >
+                <span style={{ marginRight: "5px" }}>Voir mes activités</span>
+                <BookmarkHeart size={24} />
+              </Nav.Link>
+              <Nav.Link
+                className="mx-2 my-1  btn btn-light border border-3 btn-hover-effect  rounded"
+                onClick={() => {
+                  navigate("/activities");
+                  setIsNavbarOpen(false);
+                }}
+              >
+                <span style={{ marginRight: "5px" }}>Voir les activités</span>
+                <PersonWalking size={24} />
+              </Nav.Link>
+              <Nav.Link
+                className="mx-2 my-1  btn btn-light border border-3 btn-hover-effect  rounded"
+                onClick={() => {
+                  navigate("/modify");
+                  setIsNavbarOpen(false);
+                }}
+              >
+                <span style={{ marginRight: "5px" }}>Modifier Mon Profil</span>
+                <PersonGear size={24} />
+              </Nav.Link>
+              <Nav.Link
+                className="mx-2 my-1  btn btn-light border border-3 btn-hover-effect  rounded"
+                onClick={() => {
+                  SeDeconnecter();
+                  setIsNavbarOpen(false);
+                }}
+              >
+                <span style={{ marginRight: "5px" }}>Se Déconnecter</span>
+                <BoxArrowInLeft size={24} />
+              </Nav.Link>
+            </Nav>
+            </div>
+          </Navbar.Collapse>
         </div>
-        <Navbar.Brand as={Link} to="/" className="ml-auto">
-    <img
-      src="https://valcourt2030.org/wp-content/uploads/2017/05/2030-180x67.jpg" // Replace with the path to your logo
-      width="100"
-      height="30"
-      className="d-inline-block align-top"
-      alt="Logo"
-    />
-  </Navbar.Brand>
+        <Navbar.Brand as={Link} to="/" className="ml-auto btn-hover-effect ">
+          <img
+            src="https://valcourt2030.org/wp-content/uploads/2017/05/2030-180x67.jpg" // Replace with the path to your logo
+            width="110"
+            height="30"
+            className="d-inline-block align-top me-3"
+            alt="Logo"
+          />
+        </Navbar.Brand>
       </Navbar>
     </div>
   );
